80deadd7e2e4f1835b0223e8853ef8b3a554a6ba
Mayya Matueva <mayamatueva@gmail.com>
2024-01-15 21:44:22 +0100

    detect button clicks, show and hide DOM elements

--- home.js
+++ home.js
@@ -16,3 +16,37 @@
 //  showMessage(secretCode);
 
 changePercentOff(30);
+
+/* Passing Objects to Functions
+let person = {
+    name: "John",
+    age: 32,
+    partTime: false
+};
+
+function incrementAge(p) {
+    p.age++;
+}
+
+incrementAge(person);
+
+showMessage(person.age);
+*/
+
+// let s = 'Hello';
+// showMessage(s.charAt(1));
+
+const header = document.getElementById("message");
+header.style.fontWeight = "800";
+
+const button = document.getElementById("see-review");
+button.addEventListener("click", function () {
+  const review = document.getElementById("review");
+  if (review.classList.contains("d-none")) {
+    review.classList.remove("d-none");
+    button.textContent = 'CLOSE REVIEW';
+  } else {
+    review.classList.add("d-none");
+    button.textContent = 'SEE REVIEW';
+  }
+});
